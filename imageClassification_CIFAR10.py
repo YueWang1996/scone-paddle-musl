7d7fc3f4909c3fafdcba7df8af587ed5a68c4a27
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-03-17 21:02:52 +0100

    Update imageClassification_CIFAR10.py

--- imageClassification_CIFAR10.py
+++ imageClassification_CIFAR10.py
@@ -1,58 +1,105 @@
 import paddle
 import paddle.nn.functional as F
+from paddle.vision.transforms import ToTensor
+import numpy as np
+import matplotlib.pyplot as plt
 
-from paddle.vision.transforms import Compose, Normalize
+print(paddle.__version__)
+transform = ToTensor()
+cifar10_train = paddle.vision.datasets.Cifar10(mode='train',
+                                               transform=transform)
+cifar10_test = paddle.vision.datasets.Cifar10(mode='test',
+                                              transform=transform)
+class MyNet(paddle.nn.Layer):
+    def __init__(self, num_classes=1):
+        super(MyNet, self).__init__()
 
-transform = Compose([Normalize(mean=[127.5],
-                               std=[127.5],
-                               data_format='CHW')])
-# 使用transform对数据集做归一化
-print('download training data and load training data')
-train_dataset = paddle.vision.datasets.MNIST(mode='train', transform=transform)
-test_dataset = paddle.vision.datasets.MNIST(mode='test', transform=transform)
-print('load finished')
+        self.conv1 = paddle.nn.Conv2D(in_channels=3, out_channels=32, kernel_size=(3, 3))
+        self.pool1 = paddle.nn.MaxPool2D(kernel_size=2, stride=2)
 
-class LeNet(paddle.nn.Layer):
-    def __init__(self):
-        super(LeNet, self).__init__()
-        self.conv1 = paddle.nn.Conv2D(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2)
-        self.max_pool1 = paddle.nn.MaxPool2D(kernel_size=2,  stride=2)
-        self.conv2 = paddle.nn.Conv2D(in_channels=6, out_channels=16, kernel_size=5, stride=1)
-        self.max_pool2 = paddle.nn.MaxPool2D(kernel_size=2, stride=2)
-        self.linear1 = paddle.nn.Linear(in_features=16*5*5, out_features=120)
-        self.linear2 = paddle.nn.Linear(in_features=120, out_features=84)
-        self.linear3 = paddle.nn.Linear(in_features=84, out_features=10)
+        self.conv2 = paddle.nn.Conv2D(in_channels=32, out_channels=64, kernel_size=(3,3))
+        self.pool2 = paddle.nn.MaxPool2D(kernel_size=2, stride=2)
+
+        self.conv3 = paddle.nn.Conv2D(in_channels=64, out_channels=64, kernel_size=(3,3))
+
+        self.flatten = paddle.nn.Flatten()
+
+        self.linear1 = paddle.nn.Linear(in_features=1024, out_features=64)
+        self.linear2 = paddle.nn.Linear(in_features=64, out_features=num_classes)
 
     def forward(self, x):
-        # x = x.reshape((-1, 1, 28, 28))
         x = self.conv1(x)
         x = F.relu(x)
-        x = self.max_pool1(x)
-        x = F.relu(x)
+        x = self.pool1(x)
+
         x = self.conv2(x)
-        x = self.max_pool2(x)
-        x = paddle.flatten(x, start_axis=1,stop_axis=-1)
+        x = F.relu(x)
+        x = self.pool2(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+
+        x = self.flatten(x)
         x = self.linear1(x)
         x = F.relu(x)
         x = self.linear2(x)
-        x = F.relu(x)
-        x = self.linear3(x)
         return x
+epoch_num = 10
+batch_size = 32
+learning_rate = 0.001
+val_acc_history = []
+val_loss_history = []
 
-from paddle.metric import Accuracy
-model = paddle.Model(LeNet())   # 用Model封装模型
-optim = paddle.optimizer.Adam(learning_rate=0.001, parameters=model.parameters())
+def train(model):
+    print('start training ... ')
+    # turn into training mode
+    model.train()
 
-# 配置模型
-model.prepare(
-    optim,
-    paddle.nn.CrossEntropyLoss(),
-    Accuracy()
-    )
-# 训练模型
-model.fit(train_dataset,
-        epochs=2,
-        batch_size=64,
-        verbose=1
-        )
-model.evaluate(test_dataset, batch_size=64, verbose=1)
+    opt = paddle.optimizer.Adam(learning_rate=learning_rate,
+                                parameters=model.parameters())
+
+    train_loader = paddle.io.DataLoader(cifar10_train,
+                                        shuffle=True,
+                                        batch_size=batch_size)
+
+    valid_loader = paddle.io.DataLoader(cifar10_test, batch_size=batch_size)
+
+    for epoch in range(epoch_num):
+        for batch_id, data in enumerate(train_loader()):
+            x_data = data[0]
+            y_data = paddle.to_tensor(data[1])
+            y_data = paddle.unsqueeze(y_data, 1)
+
+            logits = model(x_data)
+            loss = F.cross_entropy(logits, y_data)
+
+            if batch_id % 1000 == 0:
+                print("epoch: {}, batch_id: {}, loss is: {}".format(epoch, batch_id, loss.numpy()))
+            loss.backward()
+            opt.step()
+            opt.clear_grad()
+
+        # evaluate model after one epoch
+        model.eval()
+        accuracies = []
+        losses = []
+        for batch_id, data in enumerate(valid_loader()):
+            x_data = data[0]
+            y_data = paddle.to_tensor(data[1])
+            y_data = paddle.unsqueeze(y_data, 1)
+
+            logits = model(x_data)
+            loss = F.cross_entropy(logits, y_data)
+            acc = paddle.metric.accuracy(logits, y_data)
+            accuracies.append(acc.numpy())
+            losses.append(loss.numpy())
+
+        avg_acc, avg_loss = np.mean(accuracies), np.mean(losses)
+        print("[validation] accuracy/loss: {}/{}".format(avg_acc, avg_loss))
+        val_acc_history.append(avg_acc)
+        val_loss_history.append(avg_loss)
+        model.train()
+
+model = MyNet(num_classes=10)
+train(model)
+
