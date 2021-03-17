50649b215082e79e2c7d2756ec441cf47f7bbb8c
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-03-17 11:10:43 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -26,7 +26,7 @@ set -x -e
 
 # generate a security policy session for python app using template
 unset MRENCLAVE SCONE_CONFIG_ID
-export MRENCLAVE=$(SCONE_HASH=1 python) # get MRENCLAVE/HASH of python
+export MRENCLAVE=$(SCONE_HASH=1 python3) # get MRENCLAVE/HASH of python
 envsubst '$MRENCLAVE' < session_template.yml > session.yml
 unset MRENCLAVE SCONE_CONFIG_ID 
 
